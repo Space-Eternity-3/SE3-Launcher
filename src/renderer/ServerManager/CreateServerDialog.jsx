943796e739139986d39616efcecf93bae98406cf
Nadwey <81181783+Nadwey@users.noreply.github.com>
2023-04-25 20:42:00 +0200

    Make server config fetchable from "SE3 Api"

--- src/renderer/ServerManager/CreateServerDialog.jsx
+++ src/renderer/ServerManager/CreateServerDialog.jsx
@@ -1,6 +1,7 @@
-import { Accordion, ActionIcon, Button, Card, Group, Modal, NumberInput, Select, Space, Tabs, Text, TextInput, Title } from "@mantine/core";
+import { Accordion, ActionIcon, Button, Card, Divider, Group, Modal, NumberInput, Select, Space, Tabs, Text, TextInput, Title } from "@mantine/core";
 import { IconRefresh } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
+import ServerConfig from "./CreateServerDialog/ServerConfig";
 
 const generatePassword = (length = 20, wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') =>
     Array.from(crypto.getRandomValues(new Uint32Array(length)))
@@ -8,11 +9,13 @@ const generatePassword = (length = 20, wishlist = '0123456789ABCDEFGHIJKLMNOPQRS
         .join('')
 
 export function CreateServerDialog({ visible, serverVersions, onCancel }) {
+    const [selectedVersion, setSelectedVersion] = useState(null);
     const [adminPassword, setAdminPassword] = useState("");
-    const [serverPort, setServerPort] = useState(27683);
 
     function setRandomPassword() {
         setAdminPassword(generatePassword(20));
+        console.log(selectedVersion);
+        console.log(serverVersions.find(version => version.version === selectedVersion));
     }
 
     return <Modal size="lg" opened={visible} onClose={onCancel} centered title="Create server">
@@ -20,44 +23,36 @@ export function CreateServerDialog({ visible, serverVersions, onCancel }) {
             <TextInput
                 placeholder="My Great Server!"
                 label="Server name"
-                withAsterisk
             />
             <Select
                 label="Server version"
                 placeholder="Pick one"
-                searchable
-                withAsterisk
+                value={selectedVersion}
+                onChange={setSelectedVersion}
                 nothingFound="No options"
-                data={serverVersions.map(version => {
-                    return {
-                        label: version.name,
-                        value: version.tag
-                    };
-                })}
+                data={serverVersions.map(version => ({
+                    label: version.name,
+                    value: version.version,
+                })
+                )}
             />
         </Group>
         <Space h="sm" />
         <TextInput
+            disabled={!(serverVersions.find(version => version.version === selectedVersion))?.supportedFeatures?.includes("admin_access")}
             value={adminPassword}
             onChange={e => setAdminPassword(e.target.value)}
             placeholder="Enter a password..."
             label="Admin password"
-            withAsterisk
             rightSection={<ActionIcon title="Generate random password..." onClick={setRandomPassword}>
                 <IconRefresh size="1.125rem" />
             </ActionIcon>}
         />
+        <Space h="xs" />
+        <ServerConfig configValues={(serverVersions.find(version => version.version === selectedVersion))?.configValues} />
+
+
         <Space h="sm" />
-        <NumberInput
-            min={0}
-            max={65353}
-            placeholder="27683"
-            label="Server port"
-            withAsterisk
-        />
-
-
-        <Space h="xl" />
         <Group position="apart">
             <Button color="green">Create</Button>
         </Group>
--- src/renderer/ServerManager/CreateServerDialog/ServerConfig.jsx
+++ src/renderer/ServerManager/CreateServerDialog/ServerConfig.jsx
@@ -1,5 +1,58 @@
-import { NumberInput } from "@mantine/core";
+import { Checkbox, NumberInput, Space, Switch } from "@mantine/core";
+import { useState } from "react";
+
+function ConfigNumber({ configValue, onChange }) {
+    const [value, setValue] = useState(configValue.default_value);
+
+    function updateValue(e) {
+        onChange?.(configValue.id, e);
+        setValue(e);
+    }
+
+    return <NumberInput
+        label={configValue.name}
+        min={configValue.min}
+        max={configValue.max}
+        description={configValue.notes}
+        value={value}
+        onChange={updateValue}
+    />
+};
+
+function ConfigBoolean({ configValue, onChange  }) {
+    const [value, setValue] = useState(configValue.default_value);
+
+    function updateValue(e) {
+        onChange?.(configValue.id, e.currentTarget.checked);
+        setValue(e.currentTarget.checked);
+    }
+
+    return <Switch
+        label={configValue.name}
+        labelPosition="left"
+        checked={value}
+        onChange={updateValue}
+    />
+};
 
 export default function ServerConfig({ configValues }) {
+    return <>
+        {configValues?.map(configValue => {
+            switch (configValue.type) {
+                case "number":
+                    return <div key={configValue.id}>
+                        <ConfigNumber configValue={configValue} />
+                        <Space h="sm" />
+                    </div>
+                case "boolean":
+                    return <div key={configValue.id}>
+                        <ConfigBoolean configValue={configValue} />
+                        <Space h="sm" />
+                    </div>
 
+                default:
+                    break;
+            }
+        })}
+    </>
 };
